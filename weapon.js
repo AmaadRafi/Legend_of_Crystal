57f8fa9ee818128d947e6b5d8e871b5a26965711
Amaad Rafi <amaadrafi@gmail.com>
2019-09-15 04:05:51 -0400

    Update weapon.js
    Added basic weapons/armor - subject to change.

--- weapon.js
+++ weapon.js
@@ -24,13 +24,37 @@ var rocket = new weapon("Rocket","none","ranged",300,false);//
 var raygun = new weapon("raygun","lazer","magic",150,false);//
 var staff = new Weapon("Staff", "none", "magic", 300, false);
 var wand = new Weapon("Wand", "none", "magic", 200, false);
-var lightning_sword = new weapon("Lightningsword","none","magic",250,false);//
-
-
+var lightning_sword = new weapon("Lightningsword","none","magic",250,false);//\
 
 var iceArmor = new Armor("Ice Armor", "ice", "heavy", 1, false);
 var sheild = new Armor("Shield","metal","heavy",1,"false");
 var waterRobe = new Armor("Water Robe", "water", "robe", 30, false);
+
+
+/* Basic Weapons/Armor
+ * Amaad Rafi 
+ * 9/15/19
+ */
+
+// WARRIOR
+var schimitar = new Weapon("SCHIMITAR", "NONE", "CLOSE", 150, false);
+var heavy_sword = new Weapon("HEAVY SWORD", "NONE", "CLOSE", 200, false);
+
+// MAGE
+var ice_staff = new Weapon("ICE STAFF", "ICE", "RANGED", 200, false);
+var fire_staff = new Weapon("FIRE STAFF", "FIRE", "RANGED", 300, false);
+
+// ARCHER
+var bow = new Weapon("BOW", "NONE", "RANGED", 250, false);
+var crossbow = new Weapon("CROSSBOW", "NONE", "RANGED", 300, false);
+
+// BRAWLER - earth bender for now, but we can add more elements (think Thanos w/ gauntlet + stones(elements))
+var gauntlet = new("GAUNTLET", "EARTH", "MELEE", 200, false);
+
+// ARMOR - let's keep it simple, I don't think we need elements for armor
+var cloth_armor = new ("CLOTH ARMOR", "NONE", "LIGHT", 10, false);
+var chain_vest = new("CHAIN VEST", "NONE", "HEAVY", 25, false);
+var robe = new("ROBE", "NONE", "LIGHT", 10, false);
 
 var potion = new Item("Potion", "consumable", true);
 potion.setConsumeMessage("You used a " + potion.displayName);
