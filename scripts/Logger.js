f70c6e8e30c03bdeee7d898a1af2e0594cefb150
Jayydoodle <Jayydoodle@gmail.com>
2019-09-19 15:52:21 -0400

    9/19/19 (see change log)

---
+++ scripts/Logger.js
@@ -0,0 +1,33 @@
+/* Jason Allen 9/19/2019 2:14 AM.
+
+Script for outputting console.log
+to the game window
+
+*/
+
+(function (logger) {
+    console.old = console.log;
+    console.log = function () {
+        var output = "", arg, i;
+
+        for (i = 0; i < arguments.length; i++) {
+            arg = arguments[i];
+            output += "<span class=\"log-" + (typeof arg) + "\">";
+
+            if (
+                typeof arg === "object" &&
+                typeof JSON === "object" &&
+                typeof JSON.stringify === "function"
+            ) {
+                output += JSON.stringify(arg);   
+            } else {
+                output += arg;   
+            }
+
+            output += "</span>&nbsp;";
+        }
+
+        logger.innerHTML += output + "<br>";
+        console.old.apply(undefined, arguments);
+    };
+})(document.getElementById("console"));
