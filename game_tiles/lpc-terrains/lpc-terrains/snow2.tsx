743c45809cdaf322e107ef917721f8913f75a6c0
Shabir Ghulam <sghulam@udel.edu>
2019-10-02 14:04:23 -0400

    game maps

---
+++ game_tiles/lpc-terrains/lpc-terrains/snow2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="snow2" tilewidth="16" tileheight="16" tilecount="308" columns="22">
+ <image source="../../../../../../Downloads/snow-expansion.png" width="352" height="224"/>
+</tileset>
---
+++ game_tiles/lpc-terrains/lpc-terrains/trees.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.2" tiledversion="1.2.4" name="trees" tilewidth="16" tileheight="16" tilecount="729" columns="27">
+ <image source="../../LPC_Modifed_Art/LPC_Modifed_Art/LPC_Trees.png" width="440" height="440"/>
+</tileset>
